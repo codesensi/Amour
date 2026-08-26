e416804e7d156a17c183a94f5d1620d1632781a2
codesensi <codesensi@aliyun.com>
2026-08-26 11:06:40 +0800

    feat(project): 初始化 Spring Boot 项目结构
    - 添加了 .gitattributes 和 .gitignore 配置文件
    - 创建了基本的 Spring Boot 应用程序入口类 AmourApplication
    - 添加了基础的应用程序测试类 AmourApplicationTests
    - 配置了 application.yml 应用配置文件
    - 创建了 Gradle 构建脚本 build.gradle
    - 添加了 Gradle wrapper 配置和执行脚本
    - 包含了 Apache 2.0 许可证文件
    - 创建了项目 README.md 文件
    - 配置了 settings.gradle 项目设置文件

---
+++ src/main/java/cn/codesensi/amour/AmourApplication.java
@@ -0,0 +1,13 @@
+package cn.codesensi.amour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmourApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/codesensi/amour/AmourApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.codesensi.amour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
